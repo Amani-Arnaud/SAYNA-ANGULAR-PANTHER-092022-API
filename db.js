576f024fd497ebcac4b1239b8a3bd7b1e4f95f81
Amani-Arnaud <karnauda1er@gmail.com>
2022-09-23 20:09:17 +0000

    INITIALISATION API

---
+++ db.js
@@ -0,0 +1,19 @@
+const db_init = require('./data/db_init.json');
+const carts = require('./data/carts.json');
+const clients = require('./data/clients.json');
+const commandes = require('./data/commandes.json');
+const comments = require('./data/comments.json');
+const orders = require('./data/orders.json');
+const products = require('./data/products.json');
+const users = require('./data/users.json');
+
+module.exports = {
+    db_init,
+    carts,
+    clients,
+    commandes,
+    comments,
+    orders,
+    products,
+    users
+}
